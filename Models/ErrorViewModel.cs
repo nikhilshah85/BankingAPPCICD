8d8adc2588ca351268b7dc948ca0c2df8d2e44d4
Administrator <Administrator@E7A39BA5724953C>
2024-09-26 20:58:19 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BankingAPPCICD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
